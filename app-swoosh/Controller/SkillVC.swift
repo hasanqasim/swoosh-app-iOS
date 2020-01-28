fed2927cde91e6f61918ff32edccecc6737954ea
Hasan Qasim <hasanqasim92@gmail.com>
2020-01-28 16:14:27 +1100

    build UI, create segues, wrapup app

---
+++ app-swoosh/Controller/SkillVC.swift
@@ -0,0 +1,32 @@
+//
+//  SkillVC.swift
+//  app-swoosh
+//
+//  Created by Hasan Qasim on 28/1/20.
+//  Copyright © 2020 Muhammad Hasan. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+    
+    var player: Player!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(player.desiredLeague!)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Hasan Qasim on 28/1/20.
+//  Copyright © 2020 Muhammad Hasan. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
